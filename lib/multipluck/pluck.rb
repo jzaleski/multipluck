cd190fe3a8c562af6d4b31937b187d7e00d39aea
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2015-01-05 08:49:24 -0500

    Remove support for `opts` plus clean-up
    After careful consideration it seems unnecessary to support pluck[ing]
    and [re]maping attributes since `select` already provides the majority
    of this functionality. Adding further features to this gem does not seem
    to be worth it at this time.

--- lib/multipluck/pluck.rb
+++ lib/multipluck/pluck.rb
@@ -6,30 +6,20 @@ module Multipluck
       # Preserve the original `pluck` functionality
       alias_method :single_column_pluck, :pluck
 
-      def pluck(*args)
-        # Extract `opts` (defaults could be merged here in the future)
-        opts = args.last.is_a?(Hash) ? args.pop : {}
+      def pluck(*column_names)
+        # Yields an `Array` of `String` column-names (maintains legacy support,
+        # the previous interface expected an `Array` as the first argument)
+        column_names = Array(column_names.flatten).map(&:to_s)
 
-        # Maintains legacy support, the previous interface expected an `Array`
-        # as the first argument
-        column_names = args.flatten
+        # If there was only one `column_name` specified fall back to the default
+        # `pluck` implementation
+        return single_column_pluck(column_names.first) if column_names.size == 1
 
-        # If no `opts` is empty and there was only one `column_name` specified
-        # fall back to the default `pluck` implementation
-        return single_column_pluck(column_names.first) \
-          if column_names.size == 1 && opts.empty?
-
-        # Since `column_names` can be symbols `stringify` them here
-        column_names.map!(&:to_s)
-
-        # Yields an `Array` of `Hash(es)`
-        rows = klass.connection.select_all(select(column_names).arel)
-
-        # Row is a `Hash` of: 'column_name' => value
-        rows.map! do |row|
-          attrs = klass.initialize_attributes(row)
-          column_names.map do |col_name|
-            klass.type_cast_attribute(col_name, attrs)
+        # Fetch and map rows (yields an `Array` of `Array(s)`)
+        klass.connection.select_all(select(column_names).arel).map! do |row|
+          attributes = klass.initialize_attributes(row)
+          column_names.map do |column_name|
+            klass.type_cast_attribute(column_name, attributes)
           end
         end
       end
